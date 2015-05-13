08fdd392ce54c9e6185af499cd618aeab68657f4
Sonia Keys <skeys@cfa.harvard.edu>
2015-05-13 16:36:17 +0000

    indent -linux

--- digest2.h
+++ digest2.h
@@ -18,45 +18,45 @@
 
 // observation.
 typedef struct {
-    double mjd;                 // modified julian date
-    double ra;                  // radians
-    double dec;
-    double vmag;                // observed magnitude, normalized to V band.
-    // 0 means no magnitude.
-    int site;                   // index into site table.  basically an integer form of the
-    // MPC three character obscode.
-    _Bool spacebased;
-    double earth_observer[3];
+	double mjd;		// modified julian date
+	double ra;		// radians
+	double dec;
+	double vmag;		// observed magnitude, normalized to V band.
+	// 0 means no magnitude.
+	int site;		// index into site table.  basically an integer form of the
+	// MPC three character obscode.
+	_Bool spacebased;
+	double earth_observer[3];
 } observation;
 
 // site.  observatory site parallax constants and standard observational error
 typedef struct {
-    double longitude;           // unit is circles
-    double rhoCosPhi;           // unit is AU
-    double rhoSinPhi;
-    double obsErr;              // unit is radians
+	double longitude;	// unit is circles
+	double rhoCosPhi;	// unit is AU
+	double rhoSinPhi;
+	double obsErr;		// unit is radians
 } site;
 
 // tkstatus.  traklet status, during processing and as a result code.
 typedef enum {
-    INVALID,
-    UNPROC,
-    ACRON,
-    FAIL,
-    SUCCESS
+	INVALID,
+	UNPROC,
+	ACRON,
+	FAIL,
+	SUCCESS
 } tkstatus;
 
 typedef struct {
-    double rawScore;
-    double noIdScore;
-    _Bool tagInClass[QX][EX][IX][HX];
-    _Bool tagOutOfClass[QX][EX][IX][HX];
-    double sumAllInClass;
-    double sumUnkInClass;
-    double sumAllOutOfClass;
-    double sumUnkOutOfClass;
-    _Bool dInClass[QX][EX][IX][HX];
-    _Bool dOutOfClass[QX][EX][IX][HX];
+	double rawScore;
+	double noIdScore;
+	_Bool tagInClass[QX][EX][IX][HX];
+	_Bool tagOutOfClass[QX][EX][IX][HX];
+	double sumAllInClass;
+	double sumUnkInClass;
+	double sumAllOutOfClass;
+	double sumUnkOutOfClass;
+	_Bool dInClass[QX][EX][IX][HX];
+	_Bool dOutOfClass[QX][EX][IX][HX];
 } perClass;
 
 // tracklet.  struct holds working variables and everything associated with
@@ -64,67 +64,67 @@ typedef struct {
 //
 // note: some code relies on this structure being zero initialized.
 typedef struct {
-    // these few elements really describe the tracklet
-    tkstatus status;
-    char desig[13];
-    observation *olist;
-    int obsCap;                 // allocated capacity of olist
-    // for status=INVALID, this is the number of consecutive
-    // invalid lines.  otherwise, it is the number of valid
-    // observations in olist.
-    int lines;
+	// these few elements really describe the tracklet
+	tkstatus status;
+	char desig[13];
+	observation *olist;
+	int obsCap;		// allocated capacity of olist
+	// for status=INVALID, this is the number of consecutive
+	// invalid lines.  otherwise, it is the number of valid
+	// observations in olist.
+	int lines;
 
-    // distance independent working variables.  computed over the course
-    // of processing the tracklet.
-    observation obsPair[2];     // two obs defining motion vector
-    double obsErr[2];           // observational error for two obs
-	_Bool  noObsErr;			// true if both of above == 0
-    uint64_t rand64;            // LCG random numuber
-    double vmag;                // composite value for the tracklet
-    double sun_observer[2][3];  // vectors at times t0 and t1
-    double observer_object_unit[2][3];  // vectors at times t0 and t1
-    double dt;                  // t1 - t0
-    double invdt;               // 1/dt
-    double invdtsq;             // 1/(dt**2)
-    double soe, coe;
-    double rms;
+	// distance independent working variables.  computed over the course
+	// of processing the tracklet.
+	observation obsPair[2];	// two obs defining motion vector
+	double obsErr[2];	// observational error for two obs
+	_Bool noObsErr;		// true if both of above == 0
+	uint64_t rand64;	// LCG random numuber
+	double vmag;		// composite value for the tracklet
+	double sun_observer[2][3];	// vectors at times t0 and t1
+	double observer_object_unit[2][3];	// vectors at times t0 and t1
+	double dt;		// t1 - t0
+	double invdt;		// 1/dt
+	double invdtsq;		// 1/(dt**2)
+	double soe, coe;
+	double rms;
 
-    // distance dependent working variables.  these are computed separately
-    // for each distance considered.
-    double sun_object0[3];
-    double sun_object0_mag;
-    double sun_object0_magsq;
+	// distance dependent working variables.  these are computed separately
+	// for each distance considered.
+	double sun_object0[3];
+	double sun_object0_mag;
+	double sun_object0_magsq;
 
-    double observer_object0[3];
-    double observer_object0_mag;
+	double observer_object0[3];
+	double observer_object0_mag;
 
-    double observer1_object0[3];
-    double observer1_object0_mag;
-    double observer1_object0_magsq;
+	double observer1_object0[3];
+	double observer1_object0_mag;
+	double observer1_object0_magsq;
 
-    double tz;
-    double hmag;
-    int hmag_bin;
-    _Bool dAnyTag;
-    _Bool dTag[QX][EX][IX][HX];
+	double tz;
+	double hmag;
+	int hmag_bin;
+	_Bool dAnyTag;
+	_Bool dTag[QX][EX][IX][HX];
 
-    perClass *class;            // array, extent = nClassesComputed
+	perClass *class;	// array, extent = nClassesComputed
 } tracklet;
 
 typedef struct {
-    // various intermediate values, useful for generating statistics after
-    // the fit is done
-    int nObs;
-    double mRot[3][3];          // rotation matrix
-    double ra0;                 // ra offset
-    double (*rs)[2];            // rotated ra and dec
-    double t0;                  // time offset
-    double *ntime;              // normalized times
-    // fit solution parameters
-    double r0;
-    double rr;
-    double d0;
-    double dr;
+	// various intermediate values, useful for generating statistics after
+	// the fit is done
+	int nObs;
+	double mRot[3][3];	// rotation matrix
+	double ra0;		// ra offset
+	double (*rs)[2];	// rotated ra and dec
+	double t0;		// time offset
+	double *ntime;		// normalized times
+	// fit solution parameters
+	double r0;
+	double rr;
+	double d0;
+	double dr;
 } gcfparam;
 
 extern char *fnConfig;
@@ -164,7 +164,6 @@ extern int nClassCompute;
 extern int nClassColumns;
 extern int classCompute[D2CLASSES];
 extern int classColumn[D2CLASSES];
-
 
 // functions in d2cli.c
 FILE *openCP(char *fn, _Bool spec, char *mode);
